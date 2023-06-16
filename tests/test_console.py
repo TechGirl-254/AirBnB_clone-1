ce1c156943c3affa0977fe579811667a751eb4d0
root <TechGirl-254>
2023-06-16 07:38:52 -0700

    Modifying console.py and writing its unittest

---
+++ tests/test_console.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Testing the console module for pep8 compliance."""
+
+import unittest
+import pep8
+
+
+class test_Console(unittest.TestCase):
+    """ This class tests the compliance of the console module."""
+
+    def test_pep8_compliance(self):
+        """Runs styleguide against the module to ensure no errors exist"""
+
+        pep8styl = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['console.py'])
+        self.assertEqual(
+            result.total_errors, 0, "Found code style errors (and warnings).")
